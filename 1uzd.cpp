1d718f8350f5b6dbe78707b5e09605e353997a4b
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-02-18 12:17:57 +0200

    Struktūra

---
+++ 1uzd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string> 
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct studentas {
+    string vardas = "A";
+    string pavarde = "B";
+    vector <int> paz;
+    int egzam;
+    double rez;
+};
