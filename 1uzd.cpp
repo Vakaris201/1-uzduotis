9440193b3da7911f2c2227ef16cbe7bf98776f26
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-02-18 12:32:43 +0200

    Pridedamas duomenu ivedimas ir suskaiciuojamas galutinis balas

--- 1uzd.cpp
+++ 1uzd.cpp
@@ -5,6 +5,9 @@
 
 using std::string;
 using std::vector;
+using std::cout;
+using std::cin;
+
 
 struct studentas {
     string vardas = "A";
@@ -13,3 +16,27 @@ struct studentas {
     int egzam;
     double rez;
 };
+
+int main(){
+    studentas A;
+    vector <studentas> grupe;
+    for(int ii = 0; ii < 2; ii++)
+    {
+    cout << "Iveskite varda ir pavarde:";
+    cin >> A.vardas >> A.pavarde;
+    cout << "Iveskite semestro pazymius: \nKiek pazymiu bus? ";
+    int n, temp, sum = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cout << "Iveskite " << i+1 << " pazymi is " << n << ": ";
+        cin >> temp;
+        A.paz.push_back(temp);
+        sum += temp;
+    }
+    cout << "Iveskite egzamino pazymi: ";
+    cin >> A.egzam;
+    A.rez = sum * 1.0 / (A.paz.size() * 1.0) * 0.4 + A.egzam * 0.6;
+    grupe.push_back(A);
+    A.paz.clear();
+    }
+}
