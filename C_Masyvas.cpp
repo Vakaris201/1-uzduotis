2b1770fa9231b5760d6fb54aaaa3cc45f30c6c90
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-02-19 21:28:37 +0200

    Pridedamas duomenu ivedimas

--- C_Masyvas.cpp
+++ C_Masyvas.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int maxpaz = 100;
+const int maxstud = 10000;
 
 struct studentas {
     string vardas;
@@ -13,4 +14,30 @@ struct studentas {
     int egzam;
     double rez;
 };
+
+int main() {
+    studentas *A = new studentas[maxstud];
+    int temp, sum, m = 0;
+    char testi;
+    while(true) {
+        cout << "Iveskite varda ir pavarde: ";
+        cin >> A[m].vardas >> A[m].pavarde;
+        A[m].paz_skaicius = 0;
+        sum = 0;
+        while(true) {
+            cout << "Iveskite " << A[m].paz_skaicius + 1 << " semestro pazymi (0 - baigti): ";
+            cin >> temp;
+            if(temp == 0) break;
+            sum += temp;
+            A[m].paz[A[m].paz_skaicius] = temp;
+            A[m].paz_skaicius++;
+        }
+        cout << "Iveskite egzamino pazymi: ";
+        cin >> A[m].egzam;
+        A[m].rez = sum * 1.0/ (A[m].paz_skaicius * 1.0) * 0.4 + A[m].egzam * 0.6;
+        m++;
+        if(m >= maxstud) break;
+    }
+    delete[] A;
+}
 
