4e10c05d24060c95b13e41ea4f2ab739e86f2227
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-02-19 21:09:16 +0200

    Aprasoma struktura

---
+++ C_Masyvas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+const int maxpaz = 100;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    int paz[maxpaz];
+    int paz_skaicius;
+    int egzam;
+    double rez;
+};
+
