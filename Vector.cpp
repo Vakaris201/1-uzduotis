3f8af4ec31262845978a2f21b6119bf8e3014335
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-02-19 22:08:56 +0200

    Pradedama vector programa, aprasoma struktura

---
+++ Vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    vector<int> paz;
+    int egzam;
+    double rez;
+};
