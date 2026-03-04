e383071a85181420eabca8540226a99cdf106bec
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-03-04 22:53:25 +0200

    Klaidu gaudymo mechanizmo template'as

--- Funkcijos.h
+++ Funkcijos.h
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 using std::vector;
 using std::string;
 using std::cin;
+using std::cout;
+using std::endl;
 
 const double ND_kof = 0.4;
 const double EGZAM_kof = 0.6;
@@ -20,12 +23,32 @@ struct studentas {
     double rez;
 };
 
-template <class T>
+void clearInput();
+
+template <typename T>
 bool compare(T &a,T &b) {
     return a < b;
 }
 
-void clearInput();
+template <typename T, int Arg1 = INT_MIN, int Arg2 = INT_MAX>
+T getInput(string prompt1, string prompt2) {
+    T input;
+    while(true) {
+        try {
+            cout << prompt1;
+            cin >> input;
+            if(cin.fail() || input < Arg1 || input > Arg2) {
+                throw std::runtime_error(prompt2);
+            }
+            return input;
+        }
+        catch(std::exception& e) {
+            clearInput();
+            cout << e.what() << endl;
+            continue;
+        }
+    }
+}
 void vidurkis(studentas &S);
 void mediana(studentas &S);
 void rusiavimas(vector<studentas> &A, int sort_choice);
